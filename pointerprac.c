f521ec8481f4de3d01cec0bb09d44860612e606c
chrkk <christianalalim2005@gmail.com>
2024-12-15 22:09:13 +0800

    1st commit test

--- pointerprac.c
+++ pointerprac.c
@@ -3,27 +3,13 @@
 
 void create(int *arr, int size){
     for(int i = 0; i < size; i++){
-    printf("Enter the value of array %d: ", i);
-    scanf("%d", (arr + i));
+        printf("Enter the value of array %d: ", i);
+        scanf("%d", (arr + i));
     }
 }
 
-void display(int *arr, int size){
-    for(int i = 0; i < size;i ++){
-        printf("%d ", *(arr + i));
-    }
-}
-
-void side(int *arr, int size){
-    for(int i = 0; i < size; i++){
-        if (*(arr + i+1) > *(arr + i)){
-            printf("1");
-        }
-    }
-
-}
-
-int main(){
+int main (){
+    
     int size;
     printf("Enter size of the array: ");
     scanf("%d", &size);
@@ -31,7 +17,6 @@ int main(){
     int arr[size];
 
     create(arr,size);
-    display(arr,size);    
-    side(arr,size);
+
     return 0;
 }
